0e847b15e75191ba7425ad887bf52204c1773ba6
mistickesterio <thiagosilvadacruz163@gmail.com>
2023-03-13 22:39:03 -0300

    first commit

---
+++ Aula05_Metodos_TurmaB/metodo_sem_retorno_com_paremetro/ClasseA.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula05_Metodos_TurmaB
+{
+    class ClasseA
+    {
+        static void Main(string[] args)
+        {
+            ClasseB cb = new ClasseB();
+
+            cb.soma(200);
+
+            int k1 = 40;
+            cb.soma(k1);
+
+            Console.ReadKey();
+            Console.Clear();
+
+            // Chamada do 02º método (String)
+            cb.meu_nome("Joana da Silva");
+
+            string n = "Ricardo Medeiros";
+            cb.meu_nome(n);
+
+            Console.ReadKey(); Console.Clear();
+
+            // Chamada do 03º método (String e int)
+
+            cb.minha_idade("Ana Silva", 20);
+
+            string nome_escolhido = "Thiago Miranda";
+            int idade = 25;
+            cb.minha_idade(nome_escolhido, idade);
+
+            Console.ReadKey(); Console.Clear();
+
+            Console.ReadLine();
+
+            // Chamada do 04º método (com entrada de dados)
+
+            Console.Write("Digite o nome: ");
+            String nome = Console.ReadLine();
+
+            Console.Write("Digite o endereço: ");
+            String endereco = Console.ReadLine();
+
+            Console.Write("Digite o telefone: ");
+            String telefone = Console.ReadLine();
+
+            Console.Write("Digite o celular: ");
+            String celular = Console.ReadLine();
+
+            Console.Write("Digite o email: ");
+            String email = Console.ReadLine();
+
+            Console.Write("Digite o idade: ");
+            int idade_pessoa = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Digite o cpf: ");
+            String cpf = Console.ReadLine();
+
+            Console.Write("Digite o rg: ");
+            String rg = Console.ReadLine();
+
+            Console.Write("Digite o situação cadastro: ");
+            String status_cadastro = Console.ReadLine();
+        }
+    }
+}
---
+++ Aula05_Metodos_TurmaB/metodo_sem_retorno_com_paremetro/ClasseB.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula05_Metodos_TurmaB
+{
+    class ClasseB
+    {
+        // Métodos sem retorno e com paremetro
+
+        // 01º Exemplo - método com 1 paramêtro (INT)
+
+        public void soma(int b1)
+        {
+            int b2 = 100;
+            int res_soma = b1 + b2;
+
+            Console.WriteLine("O resultado da soma é " + res_soma);
+        }
+
+            // 02º Exemplo - método com 1 paramêtro (String)
+
+            public void meu_nome(string nome)
+            {
+                Console.WriteLine("O meu nome é " + nome);
+            }
+
+        // 03º Exemplo - método com 2 ou + paramêtros (String e int)
+
+        public void minha_idade(String nome, int idade)
+        {
+            Console.WriteLine(
+                "O meu nome é: " + nome + "\n" +
+                "Idade: " + idade); 
+        }
+
+        // 04º Exemplo - método com 2 ou + paramêtros
+
+        public void cadastro_pessoa(
+            string nome, string endereco, string telefone,
+            string celular, string email, int idade_pessoa,
+            string cpf, string rg, bool status_cadastro)
+        {
+            Console.WriteLine("Dados cadastrados\n\n");
+
+            Console.WriteLine(
+                "Nome" + nome + "\n" +
+                "Endereco" + endereco + "\n" +
+                "Telefone" + telefone + "\n" +
+                "Celular" + celular + "\n" +
+                "Email" + email + "\n" +
+                "Idade" + idade_pessoa + "\n" +
+                "CPF" + cpf + "\n" +
+                "RG" + rg + "\n" +
+                "Situação do cadastro" + status_cadastro.ToString()
+                );
+        }
+
+    }
+}
